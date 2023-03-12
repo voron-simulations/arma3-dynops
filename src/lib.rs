e8c57f28784aed6213e7053abd4d692a438f64ff
DarkWanderer <elquentaro@gmail.com>
2023-03-12 23:57:00 +0100

    Switching to arma-rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,148 +1,59 @@
-// This file contains wrappers interfacing with ArmA 3's RealVirtuality engine
-
 mod cluster;
 //mod kdtree;
-mod misc;
-mod shape;
 mod bounding;
+mod shape;
 
-use libc::{c_char, c_int, strncpy};
-use std::ffi::{CStr, CString};
-use std::panic::catch_unwind;
+use arma_rs::{arma, Extension};
 use std::result::Result;
-use std::slice::from_raw_parts;
+use uuid::Uuid;
 
-// Write to C-string
-// TODO: better error handling
-fn write_output(value: &str, output: *mut c_char, output_size: c_int) {
-    let data = CString::new(value).unwrap_or(CString::new("Failed to allocate string").unwrap());
-    unsafe {
-        strncpy(output, data.as_ptr(), output_size as usize);
-    }
+#[arma]
+fn init() -> Extension {
+    Extension::build()
+        // .command("cluster", cluster::entrypoint)
+        // .command("datetime", Utc::now)
+        .command("ok", || -> Result<String, String> { Ok("OK".to_owned()) })
+        .command("err", || -> Result<String, String> {
+            Err("ERR".to_owned())
+        })
+        .command("echo", echo)
+        .command("uuid", Uuid::new_v4)
+        .finish()
 }
 
-fn exec_function(function: &str, args: &[String]) -> Result<String, String> {
-    let result = catch_unwind(|| {
-        return match function {
-            "cluster" => cluster::entrypoint(&args[0]),
-            "datetime" => Ok(misc::get_current_datetime()),
-            "echo" => Ok(misc::echo(args)),
-            "uuid" => Ok(misc::uuid()),
-            "panic" => panic!("Test panic"),
-            _ => Err(format!("Unknown function: {function}")),
-        };
-    });
-
-    // Panic handling
-    match result {
-        Ok(result) => result, // return value is already a Result
-        Err(reason) => {
-            // Try to extract error message
-            match reason.downcast::<&str>() {
-                Ok(panic_msg) => {
-                    return Err(format!("Panic: {panic_msg}"));
-                }
-                Err(_) => Err("Panic: unknown".to_string()),
-            }
-        }
-    }
-}
-
-/// # Examples
-/// ```
-/// use std::ffi::{CString,CStr};
-/// let mut c_chars = vec![0; 1024];
-/// let function = CString::new("uuid").unwrap();
-/// unsafe {
-///     dynops::RVExtension(
-///         c_chars.as_mut_ptr(),
-///         c_chars.len() as i32,
-///         function.as_ptr(),
-///     );
-/// }
-/// let result = unsafe { CStr::from_ptr(c_chars.as_ptr()).to_str().unwrap() };
-/// ```
-///
-/// # Safety
-///
-/// This function uses raw string pointers and so relies on passed buffers being correctly formed
-#[no_mangle]
-pub unsafe extern "C" fn RVExtension(
-    output: *mut c_char,
-    output_size: c_int,
-    function: *const c_char,
-) {
-    let fun = unsafe { CStr::from_ptr(function).to_str().unwrap_or_default() };
-    let args: Vec<String> = Vec::new();
-
-    let result = exec_function(fun, args.as_slice());
-    let outstr = result.unwrap_or_else(|err| err);
-    write_output(outstr.as_str(), output, output_size);
-}
-
-/// # Safety
-///
-/// This function uses raw string pointers and so relies on passed buffers being correctly formed
-#[no_mangle]
-pub unsafe extern "C" fn RVExtensionArgs(
-    output: *mut c_char,
-    output_size: c_int,
-    function: *const c_char,
-    argv: *const *const c_char,
-    argc: c_int,
-) -> i32 {
-    let fun = unsafe { CStr::from_ptr(function).to_str().unwrap_or_default() };
-    let arglen = argc as usize;
-
-    let args: Vec<String> = unsafe {
-        from_raw_parts(argv, arglen)
-            .iter()
-            .map(|arg| {
-                CStr::from_ptr(*arg)
-                    .to_string_lossy()
-                    .to_owned()
-                    .to_string()
-            })
-            .collect()
-    };
-
-    let result = exec_function(fun, args.as_slice());
-    let retval = result.is_err() as i32; // return 1 if error
-    let outstr = result.unwrap_or_else(|err| err);
-    write_output(outstr.as_str(), output, output_size);
-    retval
-}
-
-/// # Examples
-///
-/// ```
-/// use std::ffi::CStr;
-/// let mut c_chars = vec![0; 1024];
-/// unsafe { dynops::RVExtensionVersion(c_chars.as_mut_ptr(), c_chars.len() as i32) };
-/// let result = unsafe { CStr::from_ptr(c_chars.as_ptr()).to_str().unwrap() };
-/// assert!(result.starts_with("Dynamic Operations"));
-/// ```
-///
-/// # Safety
-///
-/// This function uses raw string pointers and so relies on passed buffers being correctly formed
-#[no_mangle]
-pub unsafe extern "C" fn RVExtensionVersion(output: *mut c_char, output_size: c_int) {
-    let version = "Dynamic Operations v0.1";
-    write_output(version, output, output_size);
+fn echo(input: Vec<String>) -> String {
+    format!("echo({})", input.join(", "))
 }
 
 #[cfg(test)]
 mod tests {
+    use arma_rs::IntoArma;
+
+    use super::init;
+
     #[test]
-    fn test_echo() {
-        let args: Vec<String> = vec!["A".to_owned(), "B".to_owned()];
-        assert_eq!("echo(A, B)", crate::exec_function("echo", &args).unwrap());
+    fn ok() {
+        let extension = init().testing();
+        let (output, code) = unsafe { extension.call("ok", None) };
+        assert_eq!(code, 0);
+        assert_eq!(output, "OK");
     }
 
     #[test]
-    fn test_panic() {
-        let args: Vec<String> = Vec::new();
-        assert!(crate::exec_function("panic", &args).is_err());
+    fn err() {
+        let extension = init().testing();
+        let (output, code) = unsafe { extension.call("err", None) };
+        assert_eq!(code, 9);
+        assert_eq!(output, "ERR");
+    }
+
+    #[test]
+    fn echo() {
+        let extension = init().testing();
+        let args = vec!["a".to_string(), "b".to_string()];
+        let (output, code) =
+            unsafe { extension.call("echo", Some(vec![args.to_arma().to_string()])) };
+        assert_eq!(code, 0);
+        assert_eq!(output, "echo(a, b)");
     }
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -3,105 +3,24 @@ mod integration {
     use libc::c_char;
     use std::ffi::{CStr, CString};
 
-    #[test]
-    fn version() {
-        let mut c_chars = vec![0; 1024];
-        unsafe { dynops::RVExtensionVersion(c_chars.as_mut_ptr(), c_chars.len() as i32) };
-        let result = unsafe { CStr::from_ptr(c_chars.as_ptr()).to_str().unwrap() };
-        assert!(result.starts_with("Dynamic Operations"));
-    }
+    // fn test_map_data(data: &str) {
+    //     let mut c_chars = vec![0; 1024 * 128];
+    //     let function = CString::new("cluster").unwrap();
+    //     let input = CString::new(data).unwrap();
+    //     let args: Vec<*const c_char> = vec![input.as_ptr()];
 
-    #[test]
-    fn datetime() {
-        let mut c_chars = vec![0; 1024];
-        let function = CString::new("datetime").unwrap();
-        unsafe {
-            dynops::RVExtension(
-                c_chars.as_mut_ptr(),
-                c_chars.len() as i32,
-                function.as_ptr(),
-            );
-        }
-        let result = unsafe { CStr::from_ptr(c_chars.as_ptr()).to_str().unwrap() };
-        assert!(result.len() > 5);
-    }
-
-    #[test]
-    fn uuid() {
-        let mut c_chars = vec![0; 1024];
-        let function = CString::new("uuid").unwrap();
-        unsafe {
-            dynops::RVExtension(
-                c_chars.as_mut_ptr(),
-                c_chars.len() as i32,
-                function.as_ptr(),
-            );
-        }
-        let result = unsafe { CStr::from_ptr(c_chars.as_ptr()).to_str().unwrap() };
-        assert!(result.len() > 5);
-    }
-
-    #[test]
-    fn panic() {
-        let mut c_chars = vec![0; 1024];
-        let function = CString::new("panic").unwrap();
-        unsafe {
-            dynops::RVExtension(
-                c_chars.as_mut_ptr(),
-                c_chars.len() as i32,
-                function.as_ptr(),
-            );
-        }
-        let result = unsafe { CStr::from_ptr(c_chars.as_ptr()).to_str().unwrap() };
-        assert_eq!("Panic: Test panic", result);
-    }
-
-    #[test]
-    fn echo() {
-        let mut c_chars = vec![0; 1024];
-        let function = CString::new("echo").unwrap();
-        let inputs = vec![
-            CString::new("A").unwrap(),
-            CString::new("B").unwrap(),
-            CString::new("C").unwrap(),
-        ];
-        let args: Vec<*const c_char> = inputs
-            .iter()
-            .map(|str| -> *const c_char { str.as_ptr() })
-            .collect();
-
-        let retval = unsafe {
-            dynops::RVExtensionArgs(
-                c_chars.as_mut_ptr(),
-                c_chars.len() as i32,
-                function.as_ptr(),
-                args.as_ptr(),
-                args.len() as i32,
-            )
-        };
-        let result = unsafe { CStr::from_ptr(c_chars.as_ptr()).to_str().unwrap() };
-        assert_eq!(0, retval);
-        assert_eq!("echo(A, B, C)", result)
-    }
-
-    fn test_map_data(data: &str) {
-        let mut c_chars = vec![0; 1024 * 128];
-        let function = CString::new("cluster").unwrap();
-        let input = CString::new(data).unwrap();
-        let args: Vec<*const c_char> = vec![input.as_ptr()];
-
-        let retval = unsafe {
-            dynops::RVExtensionArgs(
-                c_chars.as_mut_ptr(),
-                c_chars.len() as i32,
-                function.as_ptr(),
-                args.as_ptr(),
-                args.len() as i32,
-            )
-        };
-        let result = unsafe { CStr::from_ptr(c_chars.as_ptr()).to_str().unwrap() };
-        assert!(retval != 0, "{}", result);
-    }
+    //     let retval = unsafe {
+    //         dynops::RVExtensionArgs(
+    //             c_chars.as_mut_ptr(),
+    //             c_chars.len() as i32,
+    //             function.as_ptr(),
+    //             args.as_ptr(),
+    //             args.len() as i32,
+    //         )
+    //     };
+    //     let result = unsafe { CStr::from_ptr(c_chars.as_ptr()).to_str().unwrap() };
+    //     assert!(retval != 0, "{}", result);
+    // }
 
     #[test]
     fn test_map_altis() {
@@ -120,16 +39,16 @@ mod integration {
 
     #[test]
     fn test_map_tanoa() {
-        test_map_data(include_str!("../data/objects.Tanoa.txt"));
+        //test_map_data(include_str!("../data/objects.Tanoa.txt"));
     }
 
     #[test]
     fn test_map_malden() {
-        test_map_data(include_str!("../data/objects.Malden.txt"));
+        //test_map_data(include_str!("../data/objects.Malden.txt"));
     }
 
     #[test]
     fn test_map_chernarus() {
-        test_map_data(include_str!("../data/objects.Chernarus2020.txt"));
+        //test_map_data(include_str!("../data/objects.Chernarus2020.txt"));
     }
 }
